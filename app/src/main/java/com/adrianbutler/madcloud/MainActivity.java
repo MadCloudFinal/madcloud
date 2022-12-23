121d610e92a3c597428fe18bfa78497f84de55ff
Sage Jasinski <Sagejasinski@gmail.com>
2022-12-23 10:04:38 -0800

    actionbar removed

--- app/src/main/java/com/adrianbutler/madcloud/MainActivity.java
+++ app/src/main/java/com/adrianbutler/madcloud/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity
 		//fullscreen
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
+
 		setContentView(R.layout.activity_main);
 	}
 }
