d3024197a8384801be2e6260759d00c07d72ed3d
Adrian Butler Jr <butlerjr123@gmail.com>
2022-12-29 12:38:59 -0600

    added shared preferences and graph ql integrations to username selection

--- app/src/main/java/com/adrianbutler/madcloud/MainActivity.java
+++ app/src/main/java/com/adrianbutler/madcloud/MainActivity.java
@@ -1,19 +1,14 @@
 package com.adrianbutler.madcloud;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.drawable.AnimationDrawable;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -24,8 +19,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.adrianbutler.madcloud.api.GraphQLManager;
+import com.adrianbutler.madcloud.auth.SharedPreferencesManager;
 import com.adrianbutler.madcloud.game.GameActivity;
-import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
     private SoundPool mSoundPool;
@@ -36,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
     private int[] soundEffectsArray;
 
+    private SharedPreferencesManager sharedPreferencesManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,22 +45,24 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+
         // Get the shared preferences object
-        SharedPreferences sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        sharedPreferencesManager = new SharedPreferencesManager(this);
 
         // Get the saved username from shared preferences
-        String username = sharedPreferences.getString("username", "");
+        String username = sharedPreferencesManager.getUsername();
 
         // If a username is saved in shared preferences, prefill the username input field with it
         EditText usernameInput = findViewById(R.id.username_input);
-        if (!username.equals("")) {
+        if (username != null) {
             usernameInput.setText(username);
         }
+
         //title sound/music
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.thunder);
         mediaPlayer.start();
 
-        
+
 //		playBtn = findViewById(R.id.landing_button_play);
 
         Button playButton = findViewById(R.id.soundFXBtn);
@@ -91,23 +93,38 @@ public class MainActivity extends AppCompatActivity {
         TextView usernameDisplay = findViewById(R.id.username_display);
         Button goToGameBtn = findViewById(R.id.TitlePlayBtn);
 
-            // Get the shared preferences object
-        SharedPreferences sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
 
             // Get the saved username from shared preferences
-        final String[] username = {sharedPreferences.getString("username", "")};
+        final String[] usernameReference = {sharedPreferencesManager.getUsername()};
 
             // If a username is saved in shared preferences, display it in place of the EditText and TextView widgets
-        if (!username[0].equals("")) {
+        if (usernameReference[0] != null) {
             usernameInput.setVisibility(View.GONE);
             usernamePrompt.setVisibility(View.GONE);
-            usernameDisplay.setText(username[0]);
+            usernameDisplay.setText(usernameReference[0]);
             usernameDisplay.setVisibility(View.VISIBLE);
             goToGameBtn.setEnabled(true);
         } else {
             // Otherwise, set an OnEditorActionListener on the EditText widget to save the inputted username to shared preferences
             usernameInput.setOnEditorActionListener((v, actionId, event) -> {
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
+
+                    usernameReference[0] = usernameInput.getText().toString();
+                    String username = usernameReference[0];
+
+
+                    String userId = GraphQLManager.createUser(username);
+
+                    if (userId == null) // user already exists in db
+                    {
+                        Toast usernameTakenToast = new Toast(this);
+                        usernameTakenToast.setText("Username is already taken");
+                        usernameTakenToast.show();
+                        return false;
+                    }
+
+                    sharedPreferencesManager.saveUser(username, userId);
+
                     // Set the text of the username display TextView to the input from the EditText widget
                     usernameDisplay.setText(usernameInput.getText().toString());
                     // Make the username display TextView visible
@@ -120,10 +137,10 @@ public class MainActivity extends AppCompatActivity {
                     goToGameBtn.setEnabled(true);
 
                     // Save the inputted username to shared preferences
-                    username[0] = usernameInput.getText().toString();
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString("username", username[0]);
-                    editor.apply();
+
+//                    SharedPreferences.Editor editor = sharedPreferences.edit();
+//                    editor.putString("username", username);
+//                    editor.apply();
 
                     return true;
                 }
