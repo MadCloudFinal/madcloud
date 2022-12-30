06b631f4b95c83e5fc495b9327f3691f34f73bff
Adrian Butler Jr <butlerjr123@gmail.com>
2022-12-29 18:56:01 -0600

    updates users high score when record is broken

--- app/src/main/java/com/adrianbutler/madcloud/game/GameOver.java
+++ app/src/main/java/com/adrianbutler/madcloud/game/GameOver.java
@@ -14,68 +14,84 @@ import com.adrianbutler.madcloud.leaderboard.LeaderboardActivity;
 import com.adrianbutler.madcloud.utils.ads.AdManager;
 import com.adrianbutler.madcloud.utils.api.GraphQLManager;
 import com.adrianbutler.madcloud.utils.auth.SharedPreferencesManager;
+import com.amplifyframework.datastore.generated.model.User;
 
-public class GameOver extends AppCompatActivity {
-    Button playAgain;
-    Button scoreBoard;
-    Button homeButton;
-    TextView yourScore;
+public class GameOver extends AppCompatActivity
+{
+	Button playAgain;
+	Button scoreBoard;
+	Button homeButton;
+	TextView yourScore;
 
-    AdManager adManager;
+	AdManager adManager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_over);
-        scoreBoard = findViewById(R.id.scoreboard_btn);
-        homeButton = findViewById(R.id.home_button);
-        yourScore = findViewById(R.id.score_text);
-        playAgain = findViewById(R.id.play_again_btn);
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game_over);
+		scoreBoard = findViewById(R.id.scoreboard_btn);
+		homeButton = findViewById(R.id.home_button);
+		yourScore = findViewById(R.id.score_text);
+		playAgain = findViewById(R.id.play_again_btn);
 
-        uiThread();
+		uiThread();
 
-        scoreBoard.setOnClickListener(view ->
-        {
-            Intent intent = new Intent(this, LeaderboardActivity.class);
-            startActivity(intent);
-        });
-
-
-        playAgain.setOnClickListener(view ->
-        {
-            Intent intent = new Intent(GameOver.this, GameActivity.class);
-            startActivity(intent);
-        });
+		scoreBoard.setOnClickListener(view ->
+		{
+			Intent intent = new Intent(this, LeaderboardActivity.class);
+			startActivity(intent);
+		});
 
 
-        homeButton.setOnClickListener(view ->
-        {
-            Intent intent = new Intent(GameOver.this, MainActivity.class);
-            startActivity(intent);
-        });
+		playAgain.setOnClickListener(view ->
+		{
+			Intent intent = new Intent(GameOver.this, GameActivity.class);
+			startActivity(intent);
+		});
 
-        yourScore.setText(getIntent().getStringExtra("score"));
 
-        int score = Integer.parseInt(getIntent().getStringExtra("score"));
+		homeButton.setOnClickListener(view ->
+		{
+			Intent intent = new Intent(GameOver.this, MainActivity.class);
+			startActivity(intent);
+		});
 
-        Log.i("GameOverActivity", "Saving user score");
+		yourScore.setText(getIntent().getStringExtra("score"));
 
-        SharedPreferencesManager sharedPreferencesManager = new SharedPreferencesManager(this);
+		int score = Integer.parseInt(getIntent().getStringExtra("score"));
 
-        sharedPreferencesManager.saveUserHighScore(score);
+		Log.i("GameOverActivity", "Saving user score");
 
-        String userId = sharedPreferencesManager.getUserId();
 
-        GraphQLManager.updateUsersHighScore(userId, score);
-    }
+		SharedPreferencesManager sharedPreferencesManager = new SharedPreferencesManager(this);
 
-    public void uiThread() {
-        runOnUiThread(() -> {
-            adManager = new AdManager(GameOver.this, false);
-            adManager.loadRewardedAd(() -> {
-                adManager.showRewardedAd(this, (() -> {
-                }));
-            });
-        });
-    }
+		String userId = sharedPreferencesManager.getUserId();
+
+		User user = GraphQLManager.findUserById(sharedPreferencesManager.getUserId());
+
+		int oldScore = user.getHighScore();
+
+		if (oldScore < score)
+		{
+			sharedPreferencesManager.saveUserHighScore(score);
+
+			GraphQLManager.updateUsersHighScore(userId, score);
+		}
+
+	}
+
+	public void uiThread()
+	{
+		runOnUiThread(() ->
+		{
+			adManager = new AdManager(GameOver.this, false);
+			adManager.loadRewardedAd(() ->
+			{
+				adManager.showRewardedAd(this, (() ->
+				{
+				}));
+			});
+		});
+	}
 }
